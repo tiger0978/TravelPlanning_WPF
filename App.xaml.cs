2a7a34d66fe858496991c842f7912106be230ff7
士雄 徐 <qazws851228@gmail.com>
2025-10-11 10:30:45 +0800

    加入專案檔案。

---
+++ App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using GoogleMap.SDK.API;
+using GoogleMap.SDK.Core;
+using GoogleMap.SDK.UI.WPF;
+using IoC_Container;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using TravelPlanning.Views;
+using Wpf.Ui.Appearance;
+
+namespace TravelPlanning
+{
+    /// <summary>
+    /// App.xaml 的互動邏輯
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var temp = ApplicationThemeManager.GetAppTheme();
+            ApplicationThemeManager.Apply(ApplicationTheme.Dark, updateAccent: false);
+            var temp2 = ApplicationThemeManager.GetAppTheme();
+            var services = new ServiceCollection();
+            services.AddGoogleMapCoreRegistration();
+            services.AddGoogleMapWPFMapRegistration();
+            services.AddTransient<Window, MainTravelWindow>();
+            services.AddTransient<Window, CreateTravelWindow>();
+            var provides = services.BuildServiceProvider();
+            var wpfs = (IEnumerable<Window>)provides.GetService(typeof(IEnumerable<Window>));
+            var wpf = wpfs.FirstOrDefault(x=>x.GetType() == typeof(CreateTravelWindow));
+            wpf.Show();
+        }
+    }
+}
---
+++ Views/CreateTravel/CreateTravelWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TravelPlanning.Views.CreateTravel;
+using Wpf.Ui.Appearance;
+
+namespace TravelPlanning.Views
+{
+    /// <summary>
+    /// CreateTravelWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class CreateTravelWindow : Window
+    {
+        public CreateTravelContext Context { get; set; } = new CreateTravelContext();    
+        public CreateTravelWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = Context;
+        }
+    }
+}
